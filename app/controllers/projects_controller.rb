f31d1bc601bc32cd0f45099a2d3fc52a8c081896
vir <vir@stowers.org>
2014-09-30 19:13:11 -0500

    working in heroku- destroy not working6_ button

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-before_action :set_widget, only: [:show, :edit, :update, :destroy]
+before_action :set_project, only: [:show, :edit, :update, :destroy]
 def new
 @project = Project.new
 end
